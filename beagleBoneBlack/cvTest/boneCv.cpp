6a6a20c1c0331f7ec4548c9c8e8c2d751cd805b0
Alex Iverson <alexjiverson@gmail.com>
2014-01-29 17:04:37 -0700

    Rewrote target detection and ranging Created python script to solve the geometric algebra equations with sympy

--- beagleBoneBlack/cvTest/boneCv.cpp
+++ beagleBoneBlack/cvTest/boneCv.cpp
@@ -3,6 +3,8 @@
  */
 
 #include<stdio.h>
+#include<string>
+#include<stdlib.h>
 #include<sys/time.h>
 #include<math.h>
 #include<iostream>
@@ -10,11 +12,12 @@
 #include<unistd.h>
 #include<opencv2/opencv.hpp>
 //#include "networktables/NetworkTable.h"
-#include "../NetTablesTest/nttools/NetworkTableTools.h"
+#include "../netTablesTest/nttools/NetworkTableTools.h"
 using namespace std;
 using namespace cv;
 
-unsigned long currentTimeMillis(){
+unsigned long currentTimeMillis()
+{
   struct timeval tv;
   gettimeofday(&tv, NULL);
   return tv.tv_sec*1000 + tv.tv_usec/1000;
@@ -26,6 +29,8 @@ pthread_cond_t frameCond;
 pthread_mutex_t frameCondLock;
 
 double pixelSqAngle(1.19e-6);
+double pixelAngle(0.001462);
+int nearVertThresh(20);
 
 int calcQuadArea(vector<Point> points) {
   int crosssum = 0;
@@ -54,7 +59,7 @@ void *readVideo(void *arg) {
     currentFrame = tempFrame;
     pthread_mutex_unlock(&framelock);
     pthread_mutex_lock(&frameCondLock);
-    cout << "signaling new frame\n";
+    //cout << "signaling new frame\n";
     pthread_cond_signal(&frameCond);
     pthread_mutex_unlock(&frameCondLock);
     usleep(50000);
@@ -63,13 +68,17 @@ void *readVideo(void *arg) {
 
 int main()
 {
-  printf("starting at %d\n", currentTimeMillis());
+  printf("starting at %lu\n", currentTimeMillis());
+  cout << "gui mode enabled\n";
   pthread_cond_init(&frameCond, NULL);
   pthread_mutex_init(&frameCondLock, NULL);
   pthread_t readerThread;
   int rc(pthread_create(&readerThread, NULL, readVideo, (void *)0));
   VideoWriter vidwrite("videoOutput.mpeg", CV_FOURCC('M', 'P', 'E', 'G'), 30, Size(800,600));
   ITable* table = NetworkTableTools::GetClientTable("10.44.99.2", "SmartDashboard");
+  namedWindow("processed_output", 1);
+  namedWindow("raw_input", 1);
+  createTrackbar("nearVertThresh", "processed_output", &nearVertThresh, 100);
   Mat frame, blue, green, red, gray, edge, draw;
   vector<Mat> planes(3);
   planes[0] = blue;
@@ -77,11 +86,11 @@ int main()
   planes[2] = red;
   double stime(currentTimeMillis());
   for (int framenum = 0; framenum < 900; ++framenum) {
-    printf("start of frame %d at %d\n", framenum, currentTimeMillis());
+    printf("start of frame %d at %lu\n", framenum, currentTimeMillis());
     pthread_mutex_lock(&frameCondLock);
-    cout << "waiting for new frame\n";
+    //cout << "waiting for new frame\n";
     pthread_cond_wait(&frameCond, &frameCondLock);
-    cout << "finished waiting for new frame\n";
+    //cout << "finished waiting for new frame\n";
     pthread_mutex_lock(&framelock);
     frame = currentFrame;
     pthread_mutex_unlock(&framelock);
@@ -90,89 +99,128 @@ int main()
       cout << "Failed to capture an image" << endl;
       return 1;
     }
-    printf("frame captured at %d\n", currentTimeMillis());
+    printf("frame captured at %lu\n", currentTimeMillis());
     //cvtColor(frame, gray, CV_BGR2GRAY);
     split(frame, planes);
     blue = planes[0];
     green = planes[1];
     red = planes[2];
-    printf("frame split into planes at %d\n", currentTimeMillis());
+    //printf("frame split into planes at %lu\n", currentTimeMillis());
     blur(green, green, Size(3,3));
     Canny(green, edge, 100, 200, 3);
-    printf("edge detection finished at %d\n", currentTimeMillis());
+    //printf("edge detection finished at %lu\n", currentTimeMillis());
     //dilate(edge, edge, getStructuringElement(MORPH_RECT, Size(5,5), Point(3,3)));
     vector<vector<Point> > contours;
     vector<Vec4i> hierarchy;
     findContours(edge, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE);
-    printf("contours found at %d\n", currentTimeMillis());
+    //printf("contours found at %lu\n", currentTimeMillis());
     vector<vector<Point> > polys(contours.size());
     for (int n = 0; n < contours.size(); ++n) {
       approxPolyDP(contours[n], polys[n], 4, true);
     }
-    printf("polygons approximated at %d\n", currentTimeMillis());
+    draw = frame.clone();
+    Scalar quadcolor(0, 255, 255);
+    //printf("polygons approximated at %lu\n", currentTimeMillis());
     vector<vector<Point> > quads;
     for (int n = 0; n < polys.size(); ++n) {
       if (polys[n].size() == 4) {
 	quads.push_back(polys[n]);
       }
     }
-    printf("quads filtered at %d\n", currentTimeMillis());
+    //printf("quads filtered at %lu\n", currentTimeMillis());
     vector<vector<Point> > rects;
     for (int n = 0; n < quads.size(); ++n) {
       vector<Point> sides(4);
+      putText(draw, "quad "+std::to_string(n), quads[n][0], FONT_HERSHEY_PLAIN, 1.0, quadcolor);
       for (int s = 0; s < 4; s++) {
 	sides[s] = quads[n][s] - quads[n][(s+1) % 4];
 	//printf("corner %d of quad %d is (%d, %d)\n", s, n, quads[n][s].x, quads[n][s].y);
 	//printf("side %d of quad %d is (%d, %d)\n", s, n, sides[s].x, sides[s].y);
       }
-      bool rectflag(true);
+      int nearvertcount(0);
+      bool sign(false);
+      bool samesign(true);
       for (int s = 0; s < 4; s++) {
-	if (abs(sides[s].ddot(sides[(s+1)%4])/norm(sides[s])/norm(sides[(s+1)%4])) > 0.25) {
-	  rectflag = false;
-	  break;
+	if (abs(sides[s].x)/abs((double)sides[s].y+1) < nearVertThresh/100.0) {
+	  ++nearvertcount;
+	}
+	int zcross(sides[s].x * sides[(s+1)%4].y - sides[s].y * sides[(s+1)%4].x);
+	if (s == 0) {
+	  sign = signbit(zcross);
+	}
+	if (signbit(zcross) != sign) {
+	  samesign = false;
 	}
       }
-      if (rectflag) {
+      if (nearvertcount == 2 & samesign) {
 	rects.push_back(quads[n]);
+	printf("quad %d is a rectangle with orientation %d\n", n, sign);
       }
     }
-    printf("rectangles filtered at %d\n", currentTimeMillis());
-    printf("%d rectangles found\n", rects.size());
-    draw = frame.clone();
+    //printf("rectangles filtered at %lu\n", currentTimeMillis());
+    printf("%lu rectangles found\n", rects.size());
     Scalar contourcolor(0, 0, 255);
     drawContours(draw, contours, -1, contourcolor);
     Scalar polycolor(255, 0, 0);
     drawContours(draw, polys, -1, polycolor);
-    Scalar quadcolor(0, 255, 255);
     drawContours(draw, quads, -1, quadcolor);
     Scalar rectcolor(0, 255, 0);
     drawContours(draw, rects, -1, rectcolor, 3);
     Scalar targetcolor(255, 255, 255);
     bool detected(false);
     double range(1e30);
+    int targetx(0), targety(0);
     for (int n = 0; n < rects.size(); ++n) {
-      Rect r1(rects[n][0], rects[n][1]), r2(rects[n][2], rects[n][3]);
-      Rect bounds = r1 | r2;
-      double ar = bounds.height / (double)bounds.width;
-      printf("rectangle %d has aspect ratio %f\n", n, ar);
-      if (ar > 0.12 & ar < 0.37) {
-	printf("target found at (%d, %d)\n", bounds.x, bounds.y);
-	double newrange(cot(sqrt(calcQuadArea(rects[n]) * pixelSqAngle)) / 9.695);
-	range = min(range, newrange);
+      vector<Point> currect(4);
+      int ydiffm1 = 0;
+      int ydiffm2 = 0;
+      int xcoord1, xcoord2;
+      for (int s = 0; s < 4; ++s) {
+	int ydiff = abs(rects[n][s].y - rects[n][(s+1)%4].y);
+	if (ydiff > ydiffm1) {
+	  ydiffm2 = ydiffm1;
+	  xcoord2 = xcoord1;
+	  ydiffm1 = ydiff;
+	  xcoord1 = rects[n][s].x;
+	} else if (ydiff > ydiffm2) {
+	  ydiffm2 = ydiff;
+	  xcoord2 = rects[n][s].x;
+	}
+      }
+      if (ydiffm1 < abs(xcoord1-xcoord2)) {
+	//double newrange((1/tan(sqrt(calcQuadArea(rects[n]) * pixelSqAngle))) * 9.695);
+	double newrange(4/tan(ydiffm1*pixelAngle));
+	printf("target found at (%d, %d) with ydiff %d and range %f\n", rects[n][0].x, rects[n][0].y, ydiffm1, newrange);
+	if (range > newrange) {
+	  range = newrange;
+	  //targetx = bounds.x;
+	  //targety = bounds.y;
+	}
 	drawContours(draw, rects, n, targetcolor, 2);
 	detected = true;
       }
     }
     if (detected) {
       table->PutNumber("Vision_Range", range);
+      //table->PutNumber("Vision_X", targetx);
+      //table->PutNumber("Vision_Y", targety);
     } else {
       table->PutNumber("Vision_Range", -1);
     }
-    printf("end of frame %d at %d\n", framenum, currentTimeMillis());
+    printf("end of frame %d at %lu\n", framenum, currentTimeMillis());
+    imshow("processed_output", draw);
+    imshow("raw_input", frame);
+    if (waitKey(10) >= 0) {
+      break;
+    }
+    //updateWindow("processed_output");
     vidwrite.write(draw);
     //imwrite("draw.png", draw);
+    usleep(50000);
   }
-  printf("finished at %d\n", currentTimeMillis());
+  printf("finished at %lu\n", currentTimeMillis());
+  while (waitKey(10) >= 0);
+  while (waitKey(10) < 0);
   //imwrite("edge.png", edge);
   //imwrite("gray.png", gray);
   //imwrite("red.png", red);
